3f16ff5a9ea0ac70d051e4f8a37fdd6261df60f1
FaridYusifzadeh <fariddsx2015@gmail.com>
2020-09-26 14:18:33 +0400

    navbar

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/*<!--FARID YUSIFZADEH https://www.linkedin.com/in/farid-yusifzadeh-434177183/-->*/
+/*========================================= START HEADER ======================================================*/
+const navSide = () => {
+    const burger = document.querySelector('.burger');
+    const nav =document.querySelector('.nav-links');
+}
